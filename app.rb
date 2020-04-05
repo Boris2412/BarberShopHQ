94599e1d0c29d21e5dd2f18257dae7012d1ce7b4
Boris Bolshakov <boris.bolsh@gmail.com>
2020-04-05 09:58:28 +0300

    Add visit page and jquery plagins

--- app.rb
+++ app.rb
@@ -11,7 +11,27 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do
   @barbers = Barber.all
+end
+
+get '/' do
   erb :index
+end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+  
+  # username, phone, datetime
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+  @color = params[:colorpicker]
+
+  erb "<h2>Спаибо, вы запиались!</h2>"
+
 end
